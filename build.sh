4b8e6f8f8dd45cc01ef32938c7b35b929b8de00d
ChiefChippy2 <chiefchippy2@gmail.com>
2023-08-02 13:27:56 +0200

    Workaround

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 wget --output-document "original.zip" 'https://clients2.google.com/service/update2/crx?response=redirect&os=linux&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=9999.0.9999.0&acceptformat=crx2,crx3&x=id%3Dekldadcjhbmljcefnilihgagmnmmlfcd%26uc'
 yes|unzip original.zip -d 'files/'
 sed -i 's/chrome/browser/g' files/*.*
-yes|cp overrides/* files/
+yes|cp -f overrides/* files/
 # remove prev build ig
 mkdir build
 rm build/leetrooms.zip
---
+++ overrides/extra.js
@@ -0,0 +1,35 @@
+browser.webRequest.onBeforeSendHeaders.addListener(addCookie, {
+  urls: ['https://api.leetrooms.com/*'],
+}, ['blocking', 'requestHeaders']);
+
+let authToken = '';
+browser.storage.local.get('authToken').then((t)=>addDynRule(t?.authToken));
+browser.storage.local.onChanged.addListener((changes) => {
+  if (changes?.authToken?.newValue) {
+    addDynRule(changes?.authToken?.newValue);
+  }
+});
+
+function addDynRule(token) {
+  authToken = token || authToken || '';
+  return;
+}
+
+function addCookie(details) {
+  const currentHeaders = details.requestHeaders;
+  currentHeaders.push({'name': 'X-Modified-By', 'value': 'Leetrooms-Firefox'});
+  if (!currentHeaders.find(x=>x.name === 'Cookie')) currentHeaders.push({'name': 'Cookie', 'value': authToken || ''});
+  return {requestHeaders: currentHeaders};
+}
+
+function snipeCookie(details) {
+  const headers = details.responseHeaders;
+  const cookie = headers.find(x=>x.name === 'set-cookie')?.value;
+  if (!cookie) return;
+  browser.storage.local.set({authToken: cookie.match(/leetrooms.sid.+?;/g)?.[0] || ''});
+  
+}
+
+browser.webRequest.onHeadersReceived.addListener(snipeCookie, {
+  urls: ['https://api.leetrooms.com/auth/*?*'],
+}, ['responseHeaders']);
